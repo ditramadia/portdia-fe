274c28e28b03b819760d37f88e4d46fc47c38e12
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-17 20:10:29 +0700

    feat: add skill cards to skill section

--- src/components/organism/Skill.tsx
+++ src/components/organism/Skill.tsx
@@ -3,8 +3,10 @@ import SkillCard from '../molecule/SkillCard';
 
 const Skill = () => {
   return (
-    <div>
+    <div className='w-11/12 md:w-fit mb-52 flex flex-col items-center gap-7 md:flex-row md:gap-12'>
       <SkillCard experience={5} title='UI/UX Designer' description='Designing a user intuitive and aesthetically pleasing interface' />
+      <SkillCard experience={3} title='Web Dev' description='Developing and maintaining website to meet user requirement' />
+      <SkillCard experience={2} title='Project Mgmt' description='Realizing projects within the set requirements and budget' />
     </div>
   );
 };
